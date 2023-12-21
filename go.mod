5d799b3a7bec28f8a9f63bf518a6c66e9a70eb98
Hussnain Jutt <huxxnainali@gmail.com>
2023-12-21 23:56:30 +0000

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/huxxnainjutt/puppy
+
+go 1.21.5
--- puppy.go
+++ puppy.go
@@ -1,1 +1,13 @@
 package puppy
+
+func bark() string {
+
+	return "woof"
+
+}
+
+func barks() string {
+
+	return "woof woof woof"
+
+}
