e9c43d979d2b855796951be1dc3cd52ad056cf1b
Hussnain Jutt <huxxnainali@gmail.com>
2023-12-22 16:38:11 +0000

    first commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/huxxnainjutt/puppy
 
 go 1.21.5
+
+require github.com/huxxnainjutt/dog v0.0.0-20231222163325-65dd589c2e90 // indirect
--- puppy.go
+++ puppy.go
@@ -1,5 +1,9 @@
 package puppy
 
+import (
+	"github.com/huxxnainjutt/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
@@ -7,3 +11,19 @@ func Bark() string {
 func Barks() string {
 	return "Woof! Woof! Woof!"
 }
+
+func BigBark () string {
+
+  return dog.WhenGrownUp(Bark())
+
+
+
+}
+
+func BigBarks () string {
+
+	return dog.WhenGrownUp(Barks())
+  
+  
+  
+  }
